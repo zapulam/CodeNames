55a46c5ff5820486fdde83f970cb41393f39519c
zapulam <zapulliam22@gmail.com>
2023-01-16 14:24:16 -0600

    switching main back to original

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-import argparse 
-from tkinter import Tk
+from tkinter import *
 from codenames import CodeNames
 
 
 if __name__ == "__main__":
-    # Args for the custom bill version 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--custom', action='store_true', help='specifies if the custom bill version should be run')
-    args = parser.parse_args()
-
     root = Tk()
     root.title("CodeNames")  
-    app = CodeNames(root, args.custom)
-    root.state('normal')
+    app = CodeNames(root)
+    root.state('zoomed')
     root.mainloop()
